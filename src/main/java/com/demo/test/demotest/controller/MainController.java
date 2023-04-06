8e2fad36a9f3e89bda19ed6a267a0721a74fa2c6
Mattia471 <cucuzzamattia47@gmail.com>
2023-04-06 12:59:18 +0200

    add description api

--- src/main/java/com/demo/test/demotest/controller/MainController.java
+++ src/main/java/com/demo/test/demotest/controller/MainController.java
@@ -22,17 +22,21 @@ public class MainController {
     @Autowired
     TotalSalesService totalSalesService;
 
+    //API che ottiene internamente il json e lo formatta nel formato richiesto
     @GetMapping(value = "json", produces = "application/json")
     public ResponseEntity<?> getJson() {
         JSONObject jsonObject = jsonFormatterService.getNewJson();
         return new ResponseEntity<>(jsonObject, HttpStatus.OK);
     }
 
+    //API che tramite una query custom ottiene i dati dal db e li restituisce
     @GetMapping(value = "total-sales", produces = "application/json")
     public ResponseEntity<List<TotalSalesResponse>> getTotalSales() {
         return new ResponseEntity<>(totalSalesService.getAllTotalSales(), HttpStatus.OK);
     }
 
+    //API che salva i dati nel db tramite una query custom
+    // possibile testing copiando il json mock/totalSalesRows.json e incollarlo nel body della richiesta
     @PostMapping(value = "save", produces = "application/json")
     public ResponseEntity<?> save(@RequestBody List<TotalSalesRequest> totalSalesRequestList) {
         totalSalesService.saveAllRows(totalSalesRequestList);
